054e69500b52343bdc98c3100b07c796e31efcbf
Loc@lTrash <105620888+Local-Trash@users.noreply.github.com>
2023-03-01 04:21:42 +0000

    adding more to the image struct

--- src/Sprite/mod.rs
+++ src/Sprite/mod.rs
@@ -1,4 +1,19 @@
-#[derive(Default)]
+use std::{path::Path, io::Cursor};
+use std::fs::read;
+
+use image::{ImageBuffer, Rgba, load, ImageFormat};
+
 pub struct Sprite {
-    nothing: String,
+    image: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    dim: (u32, u32)
+}
+
+impl Sprite {
+    pub fn new<P: AsRef<Path>>(path: P) -> Sprite {
+        let image =load(Cursor::new(&read(path).unwrap()[..]), ImageFormat::Png).unwrap().to_rgba8();
+        Self {
+            dim: image.dimensions(),
+            image
+        }
+    }
 }
