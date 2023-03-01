613cc87fe6dd626e4784bfe29b3597053f22304e
Loc@lTrash <105620888+Local-Trash@users.noreply.github.com>
2023-03-01 16:35:56 +0000

    added load functions

--- src/Sprite/mod.rs
+++ src/Sprite/mod.rs
@@ -9,9 +9,29 @@ pub struct Sprite {
 }
 
 impl Sprite {
-    pub fn new<P: AsRef<Path>>(path: P) -> Sprite {
-        let image =load(Cursor::new(&read(path).unwrap()[..]), ImageFormat::Png).unwrap().to_rgba8();
+    /// Loads an image from a path
+    pub fn fromPath<P: AsRef<Path>>(path: P) -> Sprite {
+        let image: ImageBuffer<Rgba<u8>, Vec<u8>> = load(Cursor::new(&read(path).unwrap()[..]), ImageFormat::Png).unwrap().to_rgba8();
         Self {
+            dim: image.dimensions(),
+            image
+        }
+    }
+
+    /// Loads an image from bytes
+    pub fn fromBytes(bytes: &[u8]) -> Sprite {
+        let image: ImageBuffer<Rgba<u8>, Vec<u8>> = load(Cursor::new(&bytes[..]), ImageFormat::Png).unwrap().into_rgba8();
+
+        Sprite {
+            dim: image.dimensions(),
+            image,
+        }
+    }
+
+    pub fn fromVecBytes(bytes: &Vec<u8>) -> Sprite {
+        let image: ImageBuffer<Rgba<u8>, Vec<u8>> = load(Cursor::new(&bytes[..]), ImageFormat::Png).unwrap().into_rgba8();
+
+        Sprite {
             dim: image.dimensions(),
             image
         }
